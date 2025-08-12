84cf01a5b5fec4b34217d77eb272aa6960e80c41
DonnyHuo <huocheng122828@gmail.com>
2025-08-12 23:21:36 +0800

    feat: pro init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,9 @@ import "../assets/css/globals.css";
 import "@radix-ui/themes/styles.css";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const URL = process.env.NEXT_PUBLIC_URL;
   return {
     title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-    description: "Solv Mini App - The Future of Bitcoin Finance",
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
     other: {
       "fc:frame": JSON.stringify({
         version: "1",
@@ -19,7 +18,7 @@ export async function generateMetadata(): Promise<Metadata> {
           action: {
             type: "launch_frame",
             name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-            url: URL,
+            url: process.env.NEXT_PUBLIC_URL,
             splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
             splashBackgroundColor:
               process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR
