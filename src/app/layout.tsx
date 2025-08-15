ae19b8890d98a452f0926a3242658e36d5c9cedd
DonnyHuo <huocheng122828@gmail.com>
2025-08-16 03:32:48 +0800

    feat: add openGraph and twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,39 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
     description: "Solv Mini App - The Future of Bitcoin Finance",
+    openGraph: {
+      title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      description: "Solv Mini App - The Future of Bitcoin Finance",
+      images: [
+        {
+          url: "/cover.png"
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      description: "Solv Mini App - The Future of Bitcoin Finance",
+      images: ["/cover.png"]
+    },
     other: {
       "fc:frame": JSON.stringify({
         version: "1",
+        imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
+            splashBackgroundColor:
+              process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR
+          }
+        }
+      }),
+      "fc:miniapp": JSON.stringify({
+        version: "next",
         imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
         button: {
           title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
